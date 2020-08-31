8c2dd02cf799803d327dd990f676380f0ab2a6fe
dotcs <git@dotcs.me>
2020-08-31 08:10:25 +0200

    Align color definition with tailwind spec

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'tsfresh-green-100': '#E9FBD9',
-        'tsfresh-green-200': '#CFF7B5',
-        'tsfresh-green-300': '#A9E98B',
-        'tsfresh-green-400': '#83D369',
-        'tsfresh-green-500': '#51B63C',
-        'tsfresh-green-600': '#369C2B',
-        'tsfresh-green-700': '#20831E',
-        'tsfresh-green-800': '#136918',
-        'tsfresh-green-900': '#0B5715',
+        'tsfresh-green': {
+          '100': '#E9FBD9',
+          '200': '#CFF7B5',
+          '300': '#A9E98B',
+          '400': '#83D369',
+          '500': '#51B63C',
+          '600': '#369C2B',
+          '700': '#20831E',
+          '800': '#136918',
+          '900': '#0B5715',
+        }
       }
     },
   },
