6f7443d3186ae604e236c830770b48fef4e554a8
Nils Braun <nilslennartbraun@gmail.com>
2020-09-01 23:35:27 +0200

    Fix style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,19 @@
-import '../styles/index.css'
+import "../styles/index.css";
 import "aos/dist/aos.css";
 
-import 'react-notifications/lib/notifications.css';
-import { NotificationContainer } from 'react-notifications';
+import "react-notifications/lib/notifications.css";
+import { NotificationContainer } from "react-notifications";
 
-import Head from 'next/head';
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-    <Head>
-      <title>tsfresh - Extract Features on Time Series Easily</title>
-    </Head>
-    <Component {...pageProps} />
-    <NotificationContainer />
+      <Head>
+        <title>tsfresh - Extract Features on Time Series Easily</title>
+      </Head>
+      <Component {...pageProps} />
+      <NotificationContainer />
     </>
   );
 }
