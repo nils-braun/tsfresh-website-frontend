b8992ed55fcbe4575551812acf18f1b74d1ab1c3
Nils Braun <nilslennartbraun@gmail.com>
2020-09-02 23:02:45 +0200

    Added correct URL

--- next.config.js
+++ next.config.js
@@ -1,19 +1,22 @@
-const webpack = require('webpack')
+const webpack = require("webpack");
 
-const isProd = (process.env.NODE_ENV || 'production') === 'production'
+const isProd = (process.env.NODE_ENV || "production") === "production";
 
-const assetPrefix = isProd ? '/tsfresh-website-frontend' : ''
+// keeping for later compatibility
+const assetPrefix = isProd ? "" : "";
 
 module.exports = {
   assetPrefix: assetPrefix,
-  webpack: config => {
+  webpack: (config) => {
     config.plugins.push(
       new webpack.DefinePlugin({
-        'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
-        'process.env.FRESH_API_URL': JSON.stringify("https://backend-kus76h2pea-uc.a.run.app/extraction"),
-      }),
-    )
+        "process.env.ASSET_PREFIX": JSON.stringify(assetPrefix),
+        "process.env.FRESH_API_URL": JSON.stringify(
+          "https://backend-kus76h2pea-uc.a.run.app/extraction"
+        ),
+      })
+    );
 
-    return config
+    return config;
   },
-}
+};
